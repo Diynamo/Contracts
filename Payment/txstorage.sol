a6895ffae9cd5add16c0dc0adcc36190cc7fedc9
GiuNeroniDEV <95681714+GiuNeroniDEV@users.noreply.github.com>
2022-05-17 15:28:50 +0200

    Update txstorage.sol

--- Payment/txstorage.sol
+++ Payment/txstorage.sol
@@ -43,8 +43,8 @@ library Strings {
         return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);}}
 
 contract Ownable {
-    address private _owner_1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    address private _owner_2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    address private _owner_1 = 0x330E5a355EfB830e6fD4d896FB4e4EE212c6835E;
+    address private _owner_2 = 0x330E5a355EfB830e6fD4d896FB4e4EE212c6835E;
     function get_owner_1() public view returns (address) {
         return _owner_1;}
     function get_owner_2() public view returns (address) {
@@ -66,6 +66,7 @@ contract tx_mapping is Ownable{
         uint256 qty_left;
         string hash_tx;
         string id_tx;
+        string blockchain_id;
         string method;
         uint256 tx_done;}
 
@@ -73,6 +74,7 @@ contract tx_mapping is Ownable{
         uint number_wallet;
         address address_wallet;
         string hash_tx;
+        string blockchain_id;
         string qty_bought;
         string qty_pay;
         string method;}
@@ -90,6 +92,7 @@ contract tx_mapping is Ownable{
     string tx_add;
     string tx_id_add;
     string tx_method_add;
+    string blockchain_id_add;
     uint public total_tx;
 
     wallet_data []data;
@@ -97,31 +100,32 @@ contract tx_mapping is Ownable{
     //Method -> Swappy
     //       -> Stripe
 
-    function add_wallet(uint _tx_id, address address_wallet, string memory _hashtx, string memory _quantity, string memory _amount, string memory _method) public {
-	    wallet_data memory e = wallet_data(_tx_id, address_wallet, _hashtx, _quantity, _amount, _method); 
+    function add_wallet(uint _tx_id, address address_wallet, string memory _hashtx, string memory _quantity, string memory _amount, string memory _method, string memory _blockchain_id) public {
+	    wallet_data memory e = wallet_data(_tx_id, address_wallet, _hashtx, _quantity, _amount, _method, _blockchain_id); 
   	    data.push(e);}
 
-    function get_list(uint _tx_id) public view returns(address, string memory, string memory, string memory, string memory) {
+    function get_list(uint _tx_id) public view returns(address, string memory, string memory, string memory, string memory, string memory) {
 	    uint i;
 	    for(i=0;i<data.length;i++){
   		    wallet_data memory e = data[i];
   		    if(e.number_wallet == _tx_id){
-    			return(e.address_wallet, e.hash_tx, e.qty_bought, e.qty_pay, e.method);}}}
+    			return(e.address_wallet, e.hash_tx, e.blockchain_id, e.qty_bought, e.qty_pay, e.method);}}}
     
-    function purchase (address _addr, uint _quantity, string memory _hash_tx, string memory _amount, string memory _method) public onlyOwner {
+    function purchase (address _addr, uint _quantity, string memory _hash_tx, string memory _blockchain_id, string memory _amount, string memory _method) public onlyOwner {
         quantity[_addr] = _quantity;
-        count(_addr, _hash_tx, _method);
+        count(_addr, _hash_tx, _blockchain_id, _method);
         tx_id += 1;
-        add_wallet(tx_id, _addr, _hash_tx, Strings.toString(_quantity), _amount, _method);
+        add_wallet(tx_id, _addr, _hash_tx, _blockchain_id, Strings.toString(_quantity), _amount, _method);
         if(whitelisted[_addr] == false){
             whitelist(_addr);}}
 
-    function count (address _addr, string memory _hash_tx, string memory _method) private onlyOwner {
+    function count (address _addr, string memory _hash_tx, string memory _blockchain_id, string memory _method) private onlyOwner {
         tx_done = balance[_addr].tx_done;
         total_tx += 1;
 
         if(tx_done == 0){
             balance[_addr].hash_tx = _hash_tx;
+            balance[_addr].blockchain_id = _blockchain_id;
             balance[_addr].qty_left = max_quantity;
             balance[_addr].qty_left -= quantity[_addr];
             balance[_addr].qty_bought = quantity[_addr];
@@ -131,8 +135,10 @@ contract tx_mapping is Ownable{
         if(tx_done > 0){
             tx_add = balance[_addr].hash_tx;
             tx_id_add = balance[_addr].id_tx;
+            blockchain_id_add = balance[_addr].blockchain_id;
             tx_method_add = balance[_addr].method;
             balance[_addr].hash_tx = string(abi.encodePacked(tx_add, ";", _hash_tx));
+            balance[_addr].blockchain_id = string(abi.encodePacked(blockchain_id_add, ";", _blockchain_id));
             balance[_addr].id_tx = string(abi.encodePacked(tx_id_add, ";", Strings.toString(total_tx)));
             balance[_addr].method = string(abi.encodePacked(tx_method_add, ";", _method));
             balance[_addr].qty_left -= quantity[_addr];
